f39384ec9383d1488135cc0389fe2d530871b370
Eyram Amedzor <amedzor13@gmail.com>
2020-03-26 21:54:08 +0000

    updates

--- resources/views/accounts/index.blade.php
+++ resources/views/accounts/index.blade.php
@@ -43,7 +43,7 @@
                       <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"/>
                     </svg>
                     <span>
-                      {{ $user->profile->phone ?? "No Phone no!"}}
+                      {{ $user->profile->phone ?? "Add Phone Number"}}
                     </span>
                   </button>
                 </span>
@@ -62,9 +62,9 @@
               <h3 class="text-lg leading-6 font-medium text-gray-900">
                 Personal Information
               </h3>
-              <p class="mt-1 max-w-2xl text-sm leading-5 text-gray-500">
+              <!-- <p class="mt-1 max-w-2xl text-sm leading-5 text-gray-500">
                 Use a permanent address where you can receive mail.
-              </p>
+              </p> -->
             </div>
             @php
               $titles = [
